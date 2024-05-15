703fcc4cd433e70d5912837be572506c04429b60
Angad Nagar <nagarangad03@gmail.com>
2024-05-15 21:30:26 +0530

    created navbar and footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom"
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+ 
+ export const Footer=()=>{
+   
+    return (
+        <div className="bg-gray-800"> 
+        <div className="flex justify-between items-center text-white mt-4">
+            
+            <div className="flex text-white gap-8 ml-12">
+                <div className="flex flex-col p-6 gap-2">
+                <Link to="/" className="underline hover:no-underline">SpendWise Premium</Link>
+                <Link to="/" className="underline hover:no-underline">SpendWise Silver</Link>
+                <Link to="/" className="underline hover:no-underline">SpendWise Gold</Link>
+                <Link to="/" className="underline hover:no-underline">Community</Link>
+                <Link to="/" className="underline hover:no-underline">Security</Link>
+                </div>
+                <div className="flex flex-col p-6 gap-2">
+                <Link to="/" className="underline hover:no-underline">Facebook</Link>
+                <Link to="/" className="underline hover:no-underline">Instagram</Link>
+                <Link to="/" className="underline hover:no-underline">Blog</Link>
+                <Link to="/" className="underline hover:no-underline">Contact Us</Link>
+                <Link to="/" className="underline hover:no-underline">Terms of use</Link>
+            </div>
+
+            </div>
+
+            <div className="lg:ml-auto flex items-center space-x-8 mr-32">
+                
+                <span className="text-gray-200">Social Links:</span>
+                <Link to="https://shorturl.at/nxAX2"><FaFacebook size={30} /></Link>
+                <Link to="https://shorturl.at/aIKM2"><FaInstagram size={30} /></Link>
+                <Link to="https://shorturl.at/rwCRW"><FaLinkedin size={30} /></Link>
+            </div>
+            </div>
+                <div className="flex justify-center p-4 text-gray-400">Copyright 2024 SpendWise | All Rights Reserved | SpendWise is made with ❤ by Angad.</div>
+                </div>
+    )
+ }
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 shadow-lg">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 flex items-center">
+              <Link to="/" className="text-white font-bold text-lg hover:text-green-300">
+                $pendwise
+              </Link>
+            </div>
+          </div>
+          <div className="hidden sm:block sm:ml-6">
+            <div className="flex space-x-4">
+              <Link
+                to="/dashboard"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Dashboard
+              </Link>
+
+              <Link
+                to="/"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Pricing
+              </Link>
+
+              <Link
+                to="/"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Help
+              </Link>
+
+              <Link
+                to="/"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                About us
+              </Link>
+
+              <Link
+                to="/signin"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Sign In
+              </Link>
+              <Link
+                to="/signup"
+                className="text-white hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Sign Up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
